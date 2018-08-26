0221603c92dfdaffec1433d0dd9a43035b1a93f9
derek <derek@llamabagel.ca>
2018-08-25 22:32:09 -0400

    Initial Commit

---
+++ api/Models/DbRestaurant.cs
@@ -0,0 +1,27 @@
+﻿namespace api.Models
+{
+    /// <summary>
+    /// A model of a restaurant for storing in a Database
+    /// Hours and Picture URLs are stored in seperate tables.
+    /// </summary>
+    public class DbRestaurant
+    {
+        public string Name { get; set; }
+        
+        public string Uuid { get; set; }
+        
+        public float Rating { get; set; }
+        
+        public long NumReviews { get; set; }
+        
+        public string Description { get; set; }
+        
+        public string Address { get; set; }
+        
+        public double Latitude { get; set; }
+        
+        public double Longitude { get; set; }
+        
+        public bool CheckInAllowed { get; set; }
+    }
+}
